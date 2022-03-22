7fff5a1c0d1d2c99d0d80920d9f1e1994212c5c2
bryceworth <bryceaworth@gmail.com>
2022-03-22 09:57:39 -0700

    commit tree 8e2cdcd922c4b29dbeff999947b2a013915ed1cc

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+const currentUser = `billy`
+const welcomeMessage = `Welcome to Flatbook, ${currentUser} !`
+const upperCaseCurrentUser = currentUser.toUpperCase();
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${upperCaseCurrentUser} !`
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)} !`
